e9a9114a74e3acd93d20983f006f3dc8346bb472
Björn Thalheim <bjoern.thalheim@gmx.de>
2023-03-29 10:45:17 +0200

    Add Spring Boot nature

---
+++ src/main/java/de/adesso/thalheim/gtd/CucumberSpringBootDemoApplication.java
@@ -0,0 +1,11 @@
+package de.adesso.thalheim.gtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberSpringBootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CucumberSpringBootDemoApplication.class, args);
+    }
+}
--- src/main/java/de/adesso/thalheim/gtd/Main.java
+++
@@ -1,7 +0,0 @@
-package de.adesso.thalheim.gtd;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
