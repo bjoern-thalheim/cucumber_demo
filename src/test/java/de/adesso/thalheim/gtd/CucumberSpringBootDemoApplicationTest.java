4d63a3fdc215a62745b315ed6dda68153fff3865
Björn Thalheim <bjoern.thalheim@gmx.de>
2023-03-29 10:47:00 +0200

    Add first simple @SpringBootTest.

---
+++ src/test/java/de/adesso/thalheim/gtd/CucumberSpringBootDemoApplicationTest.java
@@ -0,0 +1,11 @@
+package de.adesso.thalheim.gtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberSpringBootDemoApplicationTest {
+
+    @Test
+    void contextLoad() {}
+}
