e8a2a28c6693e6b7bb004af94a89994eebac2758
Björn Thalheim <bjoern.thalheim@gmx.de>
2023-03-29 15:00:27 +0200

    Add Cucumber scenario definition and boilerplate code.

--- src/test/java/de/adesso/thalheim/gtd/CucumberSpringBootDemoApplicationTest.java
+++ src/test/java/de/adesso/thalheim/gtd/CucumberSpringBootDemoApplicationTest.java
@@ -2,7 +2,9 @@ package de.adesso.thalheim.gtd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.cucumber.spring.CucumberContextConfiguration;
 
+@CucumberContextConfiguration
 @SpringBootTest
 class CucumberSpringBootDemoApplicationTest {
 
